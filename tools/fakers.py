c50f4342a6b889642032bbaf56849c5fb2e44777
agaj0e <artemagadzhanyan69@gmail.com>
2026-05-01 21:09:11 +0300

    auth_tests

--- tools/fakers.py
+++ tools/fakers.py
@@ -1,10 +1,9 @@
+import random
 from faker import Faker
-
 
 class Fake:
     """Класс генерации случайных тестовых данных на основе библиотеки Faker"""
     def __init__(self, faker: Faker):
-        """:param faker: экземляр класса Faker для генерации данных"""
         self.faker = faker
 
     def text(self) -> str:
@@ -37,7 +36,8 @@ class Fake:
 
     def middle_name(self) -> str:
         """Генерация отчества"""
-        return self.faker.middle_name()
+        # В ru_RU отчества доступны только с привязкой к полу
+        return random.choice([self.faker.middle_name_male(), self.faker.middle_name_female()])
 
     def estimated_time(self) -> str:
         """Генерирует строку с предполаемым временем например '2 weeks'"""
@@ -59,4 +59,4 @@ class Fake:
         return self.integer(1, 30)
 
 # Создаем экземпляр класса Fake с использованием Faker
-fake = Fake(faker=Faker())
+fake = Fake(faker=Faker('ru_RU'))
